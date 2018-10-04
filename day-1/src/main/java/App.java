eba39837e26cc5e7a63f7b6efdf78aa1cd70700d
jorchohyeah <curimax.07@gmail.com>
2018-10-03 22:34:52 -0300

    Solving problems pointed out in PR Merge e-mail

--- day-1/src/main/java/App.java
+++ day-1/src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.security.InvalidParameterException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,22 +9,39 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome fellows! Ready to get into Sticks and Luck?");
-        System.out.println("Yes or no?");
-        String ans;
-        ans = sc.nextLine().toUpperCase();
-        while (ans.equals("Y")) {
-            System.out.println("Let's start then! What is the number or players?");                 //Necesario para limpiar line
-            List<Player> players = createPlayers(sc.nextInt(),sc);
-            System.out.println("How many sticks?");
-            Container cont = initializeContainer(sc.nextInt(),sc);
-            System.out.println("Woops! Seems like :" + chooseLoser(players,cont,sc) + " has lost this match! Better luck next time!");
-            System.out.println("Wanna play again?");
-            ans = sc.next().toUpperCase();
+        System.out.println("Yes = Y or No = N?");
+        String ans = sc.nextLine().toUpperCase();
+        if (ans.equals("Y") || ans.equals("N")) {
+            while (ans.equals("Y")) {
+                System.out.println("Let's start then! What is the number or players?");
+                List<Player> players = createPlayers(sc.nextInt(), sc);
+                if (!(players.isEmpty())) {
+                    System.out.println("How many sticks?");
+                    Container cont = initializeContainer(sc.nextInt(), sc);
+                    if (cont.getInitSticks() > 0) {
+                        System.out.println("Woops! Seems like: " + chooseLoser(players, cont, sc) + " has lost this match! Better luck next time!");
+                        System.out.println("Wanna play again? Yes = Y or No = N");
+                        ans = sc.next().toUpperCase();
+                    } else {
+                        System.out.println("You've entered an invalid number of sticks. Please restart the App to play again.");
+                        break;
+                    }
+                } else {
+                    System.out.println("You've entered an invalid number of players. Please restart the App to play again.");
+                    break;
+                }
+            }
+        }else{
+            System.out.println("You've entered an invalid option. Please restart the App to play again.");
         }
         System.out.println("Have a nice day! ");
     }
-    private static List<Player> createPlayers(int cant,Scanner sc){
+
+    private static List<Player> createPlayers(int cant, Scanner sc) {
         sc.nextLine();
+        if (cant < 1) {                 //If the number provided is invalid
+            return new ArrayList<>();
+        }
         List<Player> players = new ArrayList<>();
         Player aux;
         for (int i = 0; i < cant; i++) {
@@ -33,23 +51,25 @@ public class App {
         }
         return players;
     }
-    private static Container initializeContainer(int sticks,Scanner sc){
+
+    private static Container initializeContainer(int sticks, Scanner sc) {
         Container cont = new Container(sticks);
         cont.setSticks(cont.getInitSticks());
         return cont;
     }
-    private static String chooseLoser(List<Player> players,Container cont,Scanner sc){
+
+    private static String chooseLoser(List<Player> players, Container cont, Scanner sc) {
         int x = 0;
         int sticks = 0;
         while (cont.getSticks() > 0) {
-            if (x > players.size()-1){
+            if (x > players.size() - 1) {
                 x = 0;
             }
             cont.setLast(players.get(x).getName());
             System.out.println("Now is the turn of: " + players.get(x).getName());
             System.out.println("Take 1 or 2?");
             sticks = sc.nextInt();
-            while ((sticks != 1) && (sticks != 2)){
+            while ((sticks != 1) && (sticks != 2)) {
                 System.out.println("Hey don't try to cheat! Just pick 1 or 2");
                 sticks = sc.nextInt();
             }
